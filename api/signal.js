448aea696ba70826b65205a61f4a2543e80558b1
Fahad Patwary <fahadpatwary@Fahads-Laptop-6.local>
2025-02-01 10:20:10 +0600

    Commited

--- api/signal.js
+++ api/signal.js
@@ -1,47 +1,10 @@
-const { Server } = require('socket.io');
+const socketIo = require('socket.io');
 
-export default function handler(req, res) {
+// Export the handler function for the API route
+module.exports = (req, res) => {
   if (req.method === 'GET') {
-    res.status(200).send('WebRTC Signaling Server');
+    res.status(200).send('WebRTC signaling server is running!');
   } else {
     res.status(405).send('Method Not Allowed');
   }
-}
-
-export const config = {
-  api: {
-    bodyParser: false,
-    externalResolver: true,
-  },
 };
-
-// Socket.io server setup for signaling
-const io = new Server(res.socket.server, {
-  path: '/api/socket',
-});
-
-io.on('connection', (socket) => {
-  console.log('User connected:', socket.id);
-
-  // Forward offer from one user to another
-  socket.on('offer', (offer) => {
-    console.log('Received offer:', offer);
-    socket.broadcast.emit('offer', offer);
-  });
-
-  // Forward answer from one user to another
-  socket.on('answer', (answer) => {
-    console.log('Received answer:', answer);
-    socket.broadcast.emit('answer', answer);
-  });
-
-  // Forward ICE candidates from one user to another
-  socket.on('ice-candidate', (candidate) => {
-    console.log('Received ICE candidate:', candidate);
-    socket.broadcast.emit('ice-candidate', candidate);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
-  });
-});
